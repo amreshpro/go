86930479b8e6d3d2c97abc5013631eec84dd8438
Amresh Maurya <amresh.terminal@gmail.com>
2024-02-18 21:37:10 +0530

    add type and global var

---
+++ globalVar.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// global var - first letter capital
+var MyGlobalVar int = 45
+
+func main() {
+  
+fmt.Println(MyGlobalVar)
+
+
+}
+
---
+++ type.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+
+func main() {
+  
+var isLoggedIn bool  = false
+var small int8 = 23
+var small2 int16 = 278
+var med int32 = 23232
+var large int64 = 343424242
+var defaultValue int 
+fmt.Println(defaultValue) // 0 defaultValue is zero in int 
+
+fmt.Println(small)
+fmt.Println(small2)
+fmt.Println(med)
+fmt.Println(large)
+
+fmt.Println(isLoggedIn)
+fmt.Printf("Variable type : %T \n",isLoggedIn)
+}
