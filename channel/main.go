1d83fbcf45611db41ab496d1822262a746a05750
Amresh Maurya <amresh.terminal@gmail.com>
2024-03-09 09:21:37 +0530

    channel in go

--- channel/main.go
+++ channel/main.go
@@ -7,11 +7,26 @@ import("fmt")
 // ch<-v // send v to channel ch 
 // v:=<-ch // receive from ch and assign value to v
 
-func main() {
 
-  ch:=make(chan int)
-  ch<-6
-  v:=<-ch
-  fmt.Println(v)
 
+
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to c
 }
+
+func main() {
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c // receive from c
+
+	fmt.Println(x, y, x+y)
+}
+
